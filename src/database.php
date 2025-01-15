4211f495577fe3d11bb80aed6194087287f28524
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-01-15 18:17:56 -0500

    More dynamic folder requiring
    - In .env
    - Loop files

--- src/database.php
+++ src/database.php
@@ -5,7 +5,7 @@ $f3 = \Base::instance();
 $f3->set("DB", new DB\SQL($_ENV["DB_CONNECTION_STR"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"]));
 
 $f3->set("ILGAR", [
-	"path" => "migrations/"
+	"path" => $_ENV["MIGRATIONS_DIR"]
 ]);
 
 \Chez14\Ilgar\Boot::now();
--- src/index.php
+++ src/index.php
@@ -5,13 +5,15 @@ require "../vendor/autoload.php";
 require "env.php";
 require "database.php";
 
-require "models/user.php";
+foreach (glob($_ENV["MODELS_DIR"] . "/*.php") as $ModelFile)
+	require_once $ModelFile;
 
-require "routes/main.php";
+foreach (glob($_ENV["ROUTES_DIR"] . "/*.php") as $RouteFile)
+	require_once $RouteFile;
 
 $f3 = \Base::instance();
 
-$f3->set("BASE", "/php/capstone_project");
-$f3->set("UI", "views/");
+$f3->set("BASE", $_ENV["BASE_URL"]);
+$f3->set("UI", $_ENV["VIEWS_DIR"]);
 
 $f3->run();
