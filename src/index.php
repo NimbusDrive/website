a48e784b3e72c85c44b98d6905beaa08e9cbc677
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-01-15 21:08:07 -0500

    Run format

--- src/index.php
+++ src/index.php
@@ -6,14 +6,19 @@ require "env.php";
 require "database.php";
 
 foreach (glob($_ENV["MODELS_DIR"] . "/*.php") as $ModelFile)
+{
 	require_once $ModelFile;
+}
 
 foreach (glob($_ENV["ROUTES_DIR"] . "/*.php") as $RouteFile)
+{
 	require_once $RouteFile;
+}
 
 $f3 = \Base::instance();
 
 $f3->set("BASE", $_ENV["BASE_URL"]);
 $f3->set("UI", $_ENV["VIEWS_DIR"]);
 
+$f3->clear("CACHE");
 $f3->run();
