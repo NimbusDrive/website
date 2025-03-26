81dbeb1b516a935cc26d2dabb88860adfcf47d85
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-03-26 12:55:07 -0400

    Add other drive pages with sidebar changing

--- src/routes/drive.php
+++ src/routes/drive.php
@@ -14,6 +14,28 @@ $f3->route(
 	"GET /drive/home",
 	function ($f3)
 	{
+		$f3->set("DriveTab", "Home");
+
 		echo \Template::instance()->render("drive/home.htm");
+	}
+);
+
+$f3->route(
+	"GET /drive/main",
+	function ($f3)
+	{
+		$f3->set("DriveTab", "Main");
+
+		echo \Template::instance()->render("drive/main.htm");
+	}
+);
+
+$f3->route(
+	"GET /drive/shared",
+	function ($f3)
+	{
+		$f3->set("DriveTab", "Shared");
+
+		echo \Template::instance()->render("drive/shared.htm");
 	}
 );
