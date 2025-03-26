1447ad3e1f7f97aa4f8e21e3aed3d7e3a14af398
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-03-26 14:11:26 -0400

    Begin drive dropdown switch

--- src/resources/js/drive.js
+++ src/resources/js/drive.js
@@ -1,6 +1,26 @@
-$(document).ready(() =>
+$(() =>
 {
 	$(".ui.dropdown").dropdown({
-		"action": "select"
+		"action": "select",
+		"onChange": (_, __, SelectedItems) =>
+		{
+			if (!SelectedItems || SelectedItems.length < 1) return;
+
+			let SelectedItem = SelectedItems[0];
+			if (!SelectedItem) return;
+
+			let SelectedText = SelectedItem.textContent;
+			if (!SelectedText) return;
+
+			SelectedText = SelectedText.trim();
+			if (SelectedText.length < 1) return;
+
+			console.log(SelectedText);
+
+			switch (SelectedText)
+			{
+
+			}
+		}
 	});
 });
