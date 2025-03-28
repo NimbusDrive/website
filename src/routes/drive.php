0f19b0bd788f3b68fc3d6e488daf62ee7d8284c4
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-03-28 13:20:14 -0400

    Humble file list beginnings in drive
    Also fix bad variable names in the upload route
    Also add auth requirements to drive

--- src/routes/drive.php
+++ src/routes/drive.php
@@ -5,10 +5,65 @@ use NimbusDrive\Models\File;
 
 $f3 = \Base::instance();
 
+function BuildFileHTML($Tree)
+{
+	$Result = "<div class=\"ui list\">";
+
+	foreach ($Tree as $Name => $Sub)
+	{
+		if ($Name == "_file")
+		{
+			$Result .= "<div class=\"item\"><i class=\"file icon\"></i><div class=\"content\">";
+			$Result .= "<div class=\"header\">" . htmlspecialchars($Sub["storage_path"], ENT_QUOTES, "UTF-8") . "</div>";
+			$Result .= "</div></div>";
+		}
+		else
+		{
+			$Result .= "<div class=\"item\"><i class=\"folder icon\"></i><div class=\"content\">";
+			$Result .= "<div class=\"header\">" . htmlspecialchars($Name, ENT_QUOTES, "UTF-8") . "</div>";
+			$Result .= BuildFileHTML($Sub);
+			$Result .= "</div></div>";
+		}
+	}
+
+	$Result .= "</div>";
+
+	return $Result;
+}
+
+function BuildFileList($Files)
+{
+	$Tree = [];
+
+	foreach ($Files as $File)
+	{
+		$Parts = explode("/", trim($File["storage_path"], "/"));
+		$Current = &$Tree;
+
+		foreach ($Parts as $Part)
+		{
+			if (!isset($Current[$Part]))
+				$Current[$Part] = [];
+
+			$Current = &$Current[$Part];
+		}
+
+		$Current["_file"] = $File;
+	}
+
+	return BuildFileHTML($Tree);
+}
+
 $f3->route(
 	"GET /drive",
 	function ($f3)
 	{
+		if (!$f3->exists("SESSION.user"))
+		{
+			$f3->reroute("/");
+			return;
+		}
+
 		$f3->reroute("/drive/home");
 	}
 );
@@ -17,6 +72,12 @@ $f3->route(
 	"GET /drive/home",
 	function ($f3)
 	{
+		if (!$f3->exists("SESSION.user"))
+		{
+			$f3->reroute("/");
+			return;
+		}
+
 		$f3->set("DriveTab", "Home");
 
 		echo \Template::instance()->render("drive/home.htm");
@@ -27,7 +88,16 @@ $f3->route(
 	"GET /drive/main",
 	function ($f3)
 	{
+		if (!$f3->exists("SESSION.user"))
+		{
+			$f3->reroute("/");
+			return;
+		}
+
 		$f3->set("DriveTab", "Main");
+
+		$Files = $f3->get("DB")->exec("select * from `files` where `user_id` = ?", $f3->get("SESSION.user.id"));
+		$f3->set("FileDisplay", View::instance()->raw(BuildFileList($Files)));
 
 		echo \Template::instance()->render("drive/main.htm");
 	}
@@ -37,6 +107,12 @@ $f3->route(
 	"GET /drive/shared",
 	function ($f3)
 	{
+		if (!$f3->exists("SESSION.user"))
+		{
+			$f3->reroute("/");
+			return;
+		}
+
 		$f3->set("DriveTab", "Shared");
 
 		echo \Template::instance()->render("drive/shared.htm");
@@ -49,7 +125,7 @@ $f3->route(
 	{
 		if (!$f3->exists("SESSION.user"))
 		{
-			$f3->reroute("/");
+			$f3->error(401, "Unauthorized");
 			return;
 		}
 
@@ -58,7 +134,7 @@ $f3->route(
 
 		if ($User->dry())
 		{
-			$f3->reroute("/");
+			$f3->error(401, "Unauthorized");
 			return;
 		}
 
@@ -71,30 +147,30 @@ $f3->route(
 			return;
 		}
 
-		$Files = Web::instance()->receive(function($File) use ($f3, $User)
+		$Files = Web::instance()->receive(function($UploadedFile) use ($f3, $User)
 		{
 			// TODO: File size?
 			// TODO: Extension blacklist?
 
-			$FileHash = hash_file("sha256", $File["tmp_name"]);
-			$FileExt = strtolower(pathinfo($File["name"], PATHINFO_EXTENSION));
+			$FileHash = hash_file("sha256", $UploadedFile["tmp_name"]);
+			$FileExt = strtolower(pathinfo($UploadedFile["name"], PATHINFO_EXTENSION));
 
 			$FileName = $FileHash . "." . $FileExt;
 			$Path = $_ENV["INTERNAL_STORAGE_DIR"] . DIRECTORY_SEPARATOR . $FileName;
 
-			if (!move_uploaded_file($File["tmp_name"], $Path))
+			if (!move_uploaded_file($UploadedFile["tmp_name"], $Path))
 				return false;
 
-			$File = new File($f3->get("DB"));
-			$File->user_id = $User->id;
-			$File->hash = $FileHash;
-			$File->storage_path = "C:\\"; // TODO:
-			$File->internal_path = $Path;
-			$File->status = "None";
+			$DBFile = new File($f3->get("DB"));
+			$DBFile->user_id = $User->id;
+			$DBFile->hash = $FileHash;
+			$DBFile->storage_path = "/" . basename($UploadedFile["name"]); // TODO:
+			$DBFile->internal_path = $Path;
+			$DBFile->status = "None";
 
 			try
 			{
-				$File->save();
+				$DBFile->save();
 			} catch (Exception $Exception)
 			{
 				return false;
